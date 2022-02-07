acde986e77e06e3125f822f3455fa3d70b87a935
Utkarsh Gupta <utkarsh.gupta@iongroup.com>
2022-02-07 18:31:48 +0530

    Java Code Changes

--- project/dep/src/main/java/com/company/dep/Dep.java
+++ project/dep/src/main/java/com/company/dep/Dep.java
@@ -4,13 +4,13 @@ import com.github.tomaslanger.chalk.Chalk;
 
 public class Dep {
     public void prettyPrint(String color) {
-        if (color == "Red") {
+        if (color.equals("Red")) {
             System.out.println("This is the Output in " + Chalk.on("Red").red().underline());
-        } else if (color == "Magenta") {
+        } else if (color.equals("Magenta")) {
             System.out.println("This is the Output in " + Chalk.on("Magenta").magenta().underline());
-        } else if (color == "Yellow") {
+        } else if (color.equals("Yellow")) {
             System.out.println("This is the Output in " + Chalk.on("Yellow").yellow().underline());
-        } else if (color == "Green") {
+        } else if (color.equals("Green")) {
             System.out.println("This is the Output in " + Chalk.on("Green").green().underline());
         }
 
