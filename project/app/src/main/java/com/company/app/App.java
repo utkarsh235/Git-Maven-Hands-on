7cf4addb75a258b6775c4bf827c0b8b6ae2972ae
nikhil.gautam <nikhil.gautam@iongroup.com>
2022-02-07 17:54:56 +0530

    Integrated Dep with App and build tested

--- project/app/src/main/java/com/company/app/App.java
+++ project/app/src/main/java/com/company/app/App.java
@@ -1,13 +1,19 @@
 package com.company.app;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import com.company.dep.Dep;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        Dep pn = new Dep();
+
+        System.out.println("Choose choose:- (Red/Yellow/Magenta/Green)");
+        String colour = scn.nextLine();
+
+        System.out.println(colour);
+
+        pn.prettyPrint(colour);
     }
 }
